07f31a2f56309b1df6d08f183910aadd8cc79d9a
Money Yu <abc12207@gmail.com>
2022-09-13 07:20:45 +0800

    remove unused project

--- DEMO/Module09b/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Module09b.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
